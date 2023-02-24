09354cc2b826c46e4766c815fd2d0f1b7c03a81e
Nguyen Ngoc Duong <ngocduong87@gmail.com>
2023-02-24 10:46:06 +0000

    define date

--- src/Types.hs
+++ src/Types.hs
@@ -3,8 +3,9 @@ module Types where
 
 import Data.Char (toUpper)  
 import GHC.ST (STret)
+-- import Data.Dates.Types 
 
--- DEFAULT expire in day for recruit smart contract --
+-- DEFAULT expire in day for Headhunt smart contract --
 _DEFAULT_EXPIRE_ :: Int
 _DEFAULT_EXPIRE_ = 90
 
@@ -37,10 +38,46 @@ _DEFAULT_TOKEN_ = "ADA"
 _PLATFORM_TOKEN_ :: String
 _PLATFORM_TOKEN_ = "UTC"
 
+
+
+data Wallet = Wallet {getPublicKey :: String, signPrivatekey :: String}
+
+data UserType = Company | Talent
+data User = User {getUserType :: UserType,  getUserName::String, getWallet::Wallet}
+
+type Company =  User 
+type Talent = User
+
+
+type Date = String
+
+data Job = Job {
+     getEmployeer   ::Company
+    ,getposition    ::String
+    ,getJD          ::String
+    ,getStartdate   ::Date
+    ,getExpiredate  ::Date
+    ,getJobStatus   ::JobStatus
+}
+
+
+
+data Application = Application {
+     getJob          :: Job 
+    ,getCandidate    :: Talent
+    ,getRecruitState :: RecruitState
+}
+
+
+data HeadhuntContract = HeadhuntContract {
+     getApplication :: Application 
+    ,getCompany     :: Talent
+
+    }
+
+talent = User  {getUserType = Talent,  getUserName ="Duong"}
+
+
 -- HeadHunt_contract (Application, Company, Referrer, HeadhuntReward, HHRewardToken, PlatformReward, StartDate, ContractStatus) -- quantity_to_hire (for future upgrade), )
 -- Application (job, applied_candidate, RecruitState)
 -- Job (employeeer, position, JD, startdate, expireddate, jobstatus)
-data UserType = Company | Talent
-
-data Wallet = Wallet {getPublicKey :: String, signPrivatekey :: String}
-data User = Org {getUserType :: UserType,  getUserName::String, getWallet::Wallet}
